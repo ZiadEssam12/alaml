1e941feac4a9246aa94c5500ab711b2dca48f790
ziad Essam <ziadessam1287@gmail.com>
2025-11-19 19:09:54 +0200

    fix(product revalidate path): added prisma import

--- src/app/api/dashboard/products/[id]/revalidate/route.js
+++ src/app/api/dashboard/products/[id]/revalidate/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
+import prisma from "@/lib/prisma";
 
 export async function POST(request, { params }) {
   try {
